b33ac19128ea2519bca2187cf8640c08f341284b
Dani Megert <dmegert>
2016-07-07 11:58:11 +0200

    Reduce plugin dependencies #3 - part 2

--- server/org.jboss.tools.vscode.java/src/org/jboss/tools/vscode/java/managers/ProjectsManager.java
+++ server/org.jboss.tools.vscode.java/src/org/jboss/tools/vscode/java/managers/ProjectsManager.java
@@ -1,40 +1,16 @@
 package org.jboss.tools.vscode.java.managers;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.URIUtil;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.launching.JREContainerInitializer;
-import org.eclipse.jdt.launching.JavaRuntime;
-import org.eclipse.m2e.core.embedder.MavenModelManager;
-import org.eclipse.m2e.core.internal.MavenPluginActivator;
-import org.eclipse.m2e.core.project.IMavenProjectImportResult;
-import org.eclipse.m2e.core.project.IProjectConfigurationManager;
-import org.eclipse.m2e.core.project.MavenProjectInfo;
-import org.eclipse.m2e.core.project.ProjectImportConfiguration;
 import org.jboss.tools.vscode.ipc.JsonRpcConnection;
-import org.jboss.tools.vscode.java.managers.ProjectsManager.CHANGE_TYPE;
 
 public class ProjectsManager {
 	
