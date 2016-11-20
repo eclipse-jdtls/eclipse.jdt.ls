86ecdf860a9dd09c6c1f71678e2a6d4893030ed5
Gorkem Ercan <gorkem.ercan@gmail.com>
2016-11-20 16:33:49 -0500

    Remove unused method
    Removes the obsolete method
    
    Signed-off-by: Gorkem Ercan <gorkem.ercan@gmail.com>

--- org.jboss.tools.vscode.java/src/org/jboss/tools/vscode/java/internal/handlers/JsonRpcHelpers.java
+++ org.jboss.tools.vscode.java/src/org/jboss/tools/vscode/java/internal/handlers/JsonRpcHelpers.java
@@ -10,17 +10,14 @@
  *******************************************************************************/
 package org.jboss.tools.vscode.java.internal.handlers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.jdt.core.IBuffer;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 
 final public class JsonRpcHelpers {
-	
+
 	/**
-	 * Convert line, column to a document offset.  
+	 * Convert line, column to a document offset.
 	 * @param buffer
 	 * @param line
 	 * @param column
@@ -47,7 +44,7 @@ final public class JsonRpcHelpers {
 		IDocument document = toDocument(buffer);
 		try {
 			int line = document.getLineOfOffset(offset);
-			int column = offset - document.getLineOffset(line); 
+			int column = offset - document.getLineOffset(line);
 			return new int[] {line, column};
 		} catch (BadLocationException e) {
 			// TODO Auto-generated catch block
@@ -55,29 +52,12 @@ final public class JsonRpcHelpers {
 		}
 		return null;
 	}
-	
-	public static Map<String, Object> convertRange(int startLine, int startCol, int endLine, int endCol) {
-		Map<String, Object> range = new HashMap<String, Object>();
-		Map<String, Object> start = new HashMap<String, Object>();
-		Map<String, Object> end = new HashMap<String, Object>();
-		if(startLine >-1)
-			start.put("line",startLine);
-		if(startCol > -1)
-			start.put("character", startCol);
-		if(endCol >-1)
-			end.put("character",endCol);
-		if(endLine > -1)
-			end.put("line",endLine);
-		range.put("start",start);
-		range.put("end",end);
-		return range;
-	}
 
 	/**
 	 * Returns an {@link IDocument} for the given buffer.
 	 * The implementation tries to avoid copying the buffer unless required.
 	 * The returned document may or may not be connected to the buffer.
-	 * 
+	 *
 	 * @param buffer a buffer
 	 * @return a document with the same contents as the buffer
 	 */
