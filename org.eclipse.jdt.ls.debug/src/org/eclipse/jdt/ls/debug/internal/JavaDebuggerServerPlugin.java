0b2a88a685a8b60a79227037b6c89a4e42fd35f8
Andy Xu(devdiv) <andxu@microsoft.com>
2017-07-05 10:59:11 +0800

    add initial debug plugin (#1)
    * add initial debug plugin
    
    * add debug project in language server product
    
    * add copyright, remove startup class.

---
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/JavaDebuggerServerPlugin.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+* Copyright (c) 2017 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package org.eclipse.jdt.ls.debug.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class JavaDebuggerServerPlugin implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		
+	}
+}
