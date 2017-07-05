e4a443406b9d6dc952bcfeac96d4b7aa1d79f8b7
andxu <andxu@microsoft.com>
2017-07-05 09:59:43 +0800

    add initial debug plugin

---
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/JavaDebuggerServerPlugin.java
@@ -0,0 +1,18 @@
+package org.eclipse.jdt.ls.debug.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class JavaDebuggerServerPlugin implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		
+	}
+
+}
