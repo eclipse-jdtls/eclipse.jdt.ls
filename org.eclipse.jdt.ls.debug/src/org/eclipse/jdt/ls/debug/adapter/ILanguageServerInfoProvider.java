51182d8196bbe87db31fd7e21792060c021857ef
xuzho <xuzho@microsoft.com>
2017-08-02 09:47:42 +0800

    resolve comments

--- org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/adapter/ILanguageServerInfoProvider.java
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/adapter/ILanguageServerInfoProvider.java
@@ -1,15 +1,13 @@
 package org.eclipse.jdt.ls.debug.adapter;
 
+import org.eclipse.jdt.ls.debug.internal.DebugSession;
+
 public interface ILanguageServerInfoProvider {
     String getFullyQualifiedName(String sourceFilePath, int line, int column);
 
-    String getSourceFilePath(String fullyQualifiedName, String projectName);
-
-    String getURI(String fullyQualifiedName, String projectName);
+    String getSourceFileURI(String fullyQualifiedName, DebugSession debugSession);
 
     String getSourceContents(String uri);
-
-    String[] getClassPath(String mainClassFullyQualifiedName, String projectName);
 
     com.sun.jdi.VirtualMachineManager getVirtualMachineManager();
 }
