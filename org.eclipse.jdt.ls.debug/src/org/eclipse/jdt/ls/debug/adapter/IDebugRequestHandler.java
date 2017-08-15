3a67dc61f94fee58cf191f014214b5417fa25637
Andy Xu(devdiv) <andxu@microsoft.com>
2017-08-15 13:24:30 +0800

    Add IDebugRequestHandler (#41)
    * add IDebugRequestHandler, ISettingProvider and InitializeHandler.
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * fix a interface signature change issue.
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * add a PROCESS_RESPONSE stage.
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * remove DebugRequestHandleStage
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * remove SettingProvider
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * remove setEventLater
    
    Signed-off-by: andxu <andxu@microsoft.com>
    
    * remove empty line
    
    Signed-off-by: andxu <andxu@microsoft.com>

---
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/adapter/IDebugRequestHandler.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.ls.debug.adapter;
+
+import java.util.List;
+
+public interface IDebugRequestHandler {
+    List<String> getTargetCommands();
+    
+    void handle(String command, Requests.Arguments arguments, Messages.Response response,
+                IDebugAdapterContext context);
+
+}
