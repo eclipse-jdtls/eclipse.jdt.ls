c7f8e4fb859e6f4a95308e14478febd47b649cdb
Snjezana Peco <snjezana.peco@redhat.com>
2018-01-05 18:23:02 +0100

    Add quickfix for serial version ID
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

---
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/SerialVersionSubProcessor.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Originally copied from org.eclipse.jdt.internal.ui.text.correction.SerialVersionSubProcessor
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.ls.core.internal.corrections;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.ls.core.internal.corext.fix.IProposableFix;
+import org.eclipse.jdt.ls.core.internal.corext.fix.PotentialProgrammingProblemsFix;
+import org.eclipse.jdt.ls.core.internal.corrections.proposals.CUCorrectionProposal;
+import org.eclipse.jdt.ls.core.internal.corrections.proposals.FixCorrectionProposal;
+import org.eclipse.jdt.ls.core.internal.corrections.proposals.IProposalRelevance;
+import org.eclipse.jdt.ls.core.internal.managers.ProjectsManager;
+
+/**
+ * Subprocessor for serial version quickfix proposals.
+ *
+ * @since 3.1
+ */
+public final class SerialVersionSubProcessor {
+
+	public static final class SerialVersionProposal extends FixCorrectionProposal {
+		private boolean fIsDefaultProposal;
+
+		public SerialVersionProposal(IProposableFix fix, int relevance, IInvocationContext context, boolean isDefault) {
+			super(fix, relevance, context);
+			fIsDefaultProposal = isDefault;
+		}
+
+		public boolean isDefaultProposal() {
+			return fIsDefaultProposal;
+		}
+
+		@Override
+		public String getAdditionalProposalInfo(IProgressMonitor monitor) {
+			if (fIsDefaultProposal) {
+				return CorrectionMessages.SerialVersionDefaultProposal_message_default_info;
+			} else {
+				return CorrectionMessages.SerialVersionHashProposal_message_generated_info;
+			}
+		}
+	}
+
+	/**
+	 * Determines the serial version quickfix proposals.
+	 *
+	 * @param context
+	 *            the invocation context
+	 * @param location
+	 *            the problem location
+	 * @param proposals
+	 *            the proposal collection to extend
+	 */
+	public static final void getSerialVersionProposals(final IInvocationContext context, final IProblemLocation location, final Collection<CUCorrectionProposal> proposals) {
+
+		Assert.isNotNull(context);
+		Assert.isNotNull(location);
+		Assert.isNotNull(proposals);
+
+		IProposableFix[] fixes = PotentialProgrammingProblemsFix.createMissingSerialVersionFixes(context.getASTRoot(), location);
+		if (fixes != null) {
+			proposals.add(new SerialVersionProposal(fixes[0], IProposalRelevance.MISSING_SERIAL_VERSION_DEFAULT, context, true));
+			ICompilationUnit unit = context.getCompilationUnit();
+			if (unit != null && unit.getJavaProject() != null && !ProjectsManager.DEFAULT_PROJECT_NAME.equals(unit.getJavaProject().getProject().getName())) {
+				proposals.add(new SerialVersionProposal(fixes[1], IProposalRelevance.MISSING_SERIAL_VERSION, context, false));
+			}
+		}
+	}
+}
