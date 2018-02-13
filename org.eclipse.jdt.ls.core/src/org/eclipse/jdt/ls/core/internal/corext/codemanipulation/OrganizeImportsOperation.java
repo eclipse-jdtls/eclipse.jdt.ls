75db668d72df58777139bc7ea165a71c2ae8b579
Fred Bricon <fbricon@gmail.com>
2018-02-13 12:15:49 -0500

    Fix _organize imports_ in module-info.java
    Fixes #549
    
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corext/codemanipulation/OrganizeImportsOperation.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corext/codemanipulation/OrganizeImportsOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -264,6 +264,9 @@ public class OrganizeImportsOperation {
 			if (currTypeBinding == null) {
 				if (ASTNodes.getParent(ref, ASTNode.PACKAGE_DECLARATION) != null) {
 					return true; // reference in package-info.java
+				}
+				if (ASTNodes.getParent(ref, ASTNode.MODULE_DECLARATION) != null) {
+					return true; // reference in module-info.java
 				}
 				return false; // not in a type
 			}
---
+++ org.eclipse.jdt.ls.tests/projects/eclipse/java9/src/main/java/foo/bar/MyDriverAction.java
@@ -0,0 +1,12 @@
+package foo.bar;
+
+import java.sql.DriverAction;
+
+public class MyDriverAction implements DriverAction {
+
+	@Override
+	public void deregister() {
+		
+	}
+	
+}
