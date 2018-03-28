353e3c3fa108fb55b1fe419fef691319a0fb5388
Fred Bricon <fbricon@gmail.com>
2018-03-27 20:48:07 -0400

    Don't propose to import test classes in main code
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/JavaProjectHelper.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Ferenc Hechler, ferenc_hechler@users.sourceforge.net - 83258 [jar exporter] Deploy java application as executable jar
+ *******************************************************************************/
+package org.eclipse.jdt.ls.core.internal;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IClasspathAttribute;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+
+/**
+ * Helper methods to set up a IJavaProject.
+ *
+ * Contains bits copied from org.eclipse.jdt.testplugin.JavaProjectHelper.java
+ */
+public class JavaProjectHelper {
+
+	/**
+	 * Adds a source container to a IJavaProject.
+	 *
+	 * @param jproject
+	 *                             The parent project
+	 * @param containerName
+	 *                             The name of the new source container
+	 * @param inclusionFilters
+	 *                             Inclusion filters to set
+	 * @param exclusionFilters
+	 *                             Exclusion filters to set
+	 * @param outputLocation
+	 *                             The location where class files are written to,
+	 *                             <b>null</b> for project output folder
+	 * @param attributes
+	 *                             The classpath attributes to set
+	 * @return The handle to the new source container
+	 * @throws CoreException
+	 *                           Creation failed
+	 */
+	public static IPackageFragmentRoot addSourceContainer(IJavaProject jproject, String containerName, IPath[] inclusionFilters, IPath[] exclusionFilters, String outputLocation, IClasspathAttribute[] attributes) throws CoreException {
+		IProject project = jproject.getProject();
+		IContainer container = null;
+		if (containerName == null || containerName.length() == 0) {
+			container = project;
+		} else {
+			IFolder folder = project.getFolder(containerName);
+			if (!folder.exists()) {
+				JDTUtils.createFolders(folder, null);
+			}
+			container = folder;
+		}
+		IPackageFragmentRoot root = jproject.getPackageFragmentRoot(container);
+
+		IPath outputPath = null;
+		if (outputLocation != null) {
+			IFolder folder = project.getFolder(outputLocation);
+			if (!folder.exists()) {
+				JDTUtils.createFolders(folder, null);
+			}
+			outputPath = folder.getFullPath();
+		}
+		IClasspathEntry cpe = JavaCore.newSourceEntry(root.getPath(), inclusionFilters, exclusionFilters, outputPath, attributes);
+		addToClasspath(jproject, cpe);
+		return root;
+	}
+
+	public static void addToClasspath(IJavaProject jproject, IClasspathEntry cpe) throws JavaModelException {
+		IClasspathEntry[] oldEntries = jproject.getRawClasspath();
+		for (int i = 0; i < oldEntries.length; i++) {
+			if (oldEntries[i].equals(cpe)) {
+				return;
+			}
+		}
+		int nEntries = oldEntries.length;
+		IClasspathEntry[] newEntries = new IClasspathEntry[nEntries + 1];
+		System.arraycopy(oldEntries, 0, newEntries, 0, nEntries);
+		newEntries[nEntries] = cpe;
+		jproject.setRawClasspath(newEntries, null);
+	}
+}
