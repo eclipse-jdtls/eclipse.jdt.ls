b8ed7e79ea1a4dc7660249038eb35f776af92a6c
Roland Grunberg <rgrunber@redhat.com>
2018-07-25 10:20:42 -0400

    Use NecessaryParenthesesChecker from o.e.jdt.core.manipulation.
    - With http://eclip.se/537247, NecessaryParenthesesChecker is now
    available from org.eclipse.jdt.core.manipulation
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corext/codemanipulation/GetterSetterUtil.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corext/codemanipulation/GetterSetterUtil.java
@@ -37,10 +37,10 @@ import org.eclipse.jdt.core.dom.PostfixExpression;
 import org.eclipse.jdt.core.dom.PrefixExpression;
 import org.eclipse.jdt.core.dom.PrimitiveType;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.internal.core.manipulation.dom.NecessaryParenthesesChecker;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 import org.eclipse.jdt.internal.corext.util.JdtFlags;
-import org.eclipse.jdt.ls.core.internal.corext.dom.NecessaryParenthesesChecker;
 
 public class GetterSetterUtil {
 
