879a7704c38430101f50dad78fdfd9d657653566
Thomas Mäder <tsmaeder@users.noreply.github.com>
2018-09-05 21:43:07 +0200

    Allow to send custom notifications (#779)
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/lsp/ExecuteCommandProposedClient.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/lsp/ExecuteCommandProposedClient.java
@@ -13,11 +13,15 @@ package org.eclipse.jdt.ls.core.internal.lsp;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4j.ExecuteCommandParams;
+import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
 import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
 
 public interface ExecuteCommandProposedClient {
 
 	@JsonRequest("workspace/executeClientCommand")
 	CompletableFuture<Object> executeClientCommand(ExecuteCommandParams params);
+
+	@JsonNotification("workspace/notify")
+	void sendNotification(ExecuteCommandParams params);
 
 }
