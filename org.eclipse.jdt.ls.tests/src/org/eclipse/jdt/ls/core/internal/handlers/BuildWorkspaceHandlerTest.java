b0470b645f7eccab8f1938c9c95106ceb1420619
Snjezana Peco <snjezana.peco@redhat.com>
2018-10-15 22:30:37 +0200

    [regression] Maven diagnostics show up and disappear on save
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/BuildWorkspaceHandlerTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/BuildWorkspaceHandlerTest.java
@@ -19,6 +19,8 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.ls.core.internal.BuildWorkspaceStatus;
 import org.eclipse.jdt.ls.core.internal.JavaClientConnection;
 import org.eclipse.jdt.ls.core.internal.JavaClientConnection.JavaLanguageClient;
@@ -31,34 +33,35 @@ public class BuildWorkspaceHandlerTest extends AbstractProjectsManagerBasedTest 
 	private JavaClientConnection javaClient = new JavaClientConnection(client);
 
 	private BuildWorkspaceHandler handler;
-	private IFile file;
+	private IProject project;
 
 	@Before
 	public void setUp() throws Exception {
-		handler = new BuildWorkspaceHandler(javaClient, projectsManager, new WorkspaceDiagnosticsHandler(javaClient, projectsManager));
+		handler = new BuildWorkspaceHandler(javaClient, projectsManager);
 		importProjects("maven/salut2");
-		file = linkFilesToDefaultProject("singlefile/Single.java");
+		project = ResourcesPlugin.getWorkspace().getRoot().getProject("salut2");
 	}
 
 	@Test
 	public void testSucceedCase() throws Exception {
 		BuildWorkspaceStatus result = handler.buildWorkspace(false, monitor);
-		waitForBackgroundJobs();
 		assertTrue(String.format("BuildWorkspaceStatus is: %s.", result.toString()), result == BuildWorkspaceStatus.SUCCEED);
 	}
 
 	@Test
 	public void testFailedCase() throws Exception {
-		String codeWithError = "public class Single2 {\n" +
+		//@formatter:off
+		String codeWithError = "package foo;\n" +
+				"	public class Single2 {\n" +
 				"	public static void main(String[] args){\n"+
 				"		int ss = 1;;\n"+
 				"	}\n"+
 				"}";
+		//@formatter:on
+		IFile file = project.getFile("src/main/java/foo/Bar.java");
 		try (InputStream stream = new ByteArrayInputStream(codeWithError.getBytes(StandardCharsets.UTF_8))) {
 			file.setContents(stream, true, false, monitor);
-			waitForBackgroundJobs();
 			BuildWorkspaceStatus result = handler.buildWorkspace(false, monitor);
-			waitForBackgroundJobs();
 			assertEquals(result, BuildWorkspaceStatus.WITH_ERROR);
 		}
 	}
@@ -67,7 +70,6 @@ public class BuildWorkspaceHandlerTest extends AbstractProjectsManagerBasedTest 
 	public void testCanceledCase() throws Exception {
 		monitor.setCanceled(true);
 		BuildWorkspaceStatus result = handler.buildWorkspace(false, monitor);
-		waitForBackgroundJobs();
 		assertEquals(result, BuildWorkspaceStatus.CANCELLED);
 	}
 }
