978ae909ba5f3c1623bfeb9caedce2e34ca379ae
Sheng Chen <sheche@microsoft.com>
2019-07-25 16:07:30 +0800

    Address comments
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/refactoring/InvertVariableTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/refactoring/InvertVariableTest.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat Inc. and others.
+ * Copyright (c) 2019 Microsoft Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Red Hat Inc. - initial API and implementation
+ *     Microsoft Corporation - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.jdt.ls.core.internal.refactoring;
 
 import java.util.Hashtable;
