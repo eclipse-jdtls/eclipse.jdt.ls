628a1dd266ca1c2c50d343cbdfdc60e316778ccb
Sheng Chen <sheche@microsoft.com>
2020-05-17 16:00:13 +0800

    Expand the API usages
    - Notifications will be sent to client for all kinds of projects when classpath get updated.
    - Project's root uri is also accepted to query for the project information
    
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/ClasspathUpdateHandlerTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/ClasspathUpdateHandlerTest.java
@@ -13,28 +13,35 @@
 
 package org.eclipse.jdt.ls.core.internal.handlers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.ls.core.internal.ActionableNotification;
 import org.eclipse.jdt.ls.core.internal.JavaClientConnection;
 import org.eclipse.jdt.ls.core.internal.ResourceUtils;
-import org.eclipse.jdt.ls.core.internal.managers.AbstractProjectsManagerBasedTest;
+import org.eclipse.jdt.ls.core.internal.managers.AbstractInvisibleProjectBasedTest;
+import org.eclipse.jdt.ls.core.internal.managers.InvisibleProjectBuildSupport;
+import org.eclipse.jdt.ls.core.internal.managers.ProjectsManager.CHANGE_TYPE;
+import org.eclipse.jdt.ls.core.internal.preferences.Preferences.FeatureStatus;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ClasspathUpdateHandlerTest extends AbstractProjectsManagerBasedTest {
+public class ClasspathUpdateHandlerTest extends AbstractInvisibleProjectBasedTest {
 	@Mock
 	private JavaClientConnection connection;
 
@@ -43,7 +50,8 @@ public class ClasspathUpdateHandlerTest extends AbstractProjectsManagerBasedTest
 	@Before
 	public void setup() throws Exception {
 		handler = new ClasspathUpdateHandler(connection);
-		handler.addElementChangeListener();;
+		handler.addElementChangeListener();
+		preferences.setUpdateBuildConfigurationStatus(FeatureStatus.automatic);
 	}
 
 	@After
@@ -54,16 +62,74 @@ public class ClasspathUpdateHandlerTest extends AbstractProjectsManagerBasedTest
 	}
 
 	@Test
-	public void testClasspathUpdate() throws Exception {
-		//import project
+	public void testClasspathUpdateForMaven() throws Exception {
 		importProjects("maven/salut");
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("salut");
 		IFile pom = project.getFile("/pom.xml");
 		assertTrue(pom.exists());
 		ResourceUtils.setContent(pom, ResourceUtils.getContent(pom).replaceAll("<version>3.5</version>", "<version>3.6</version>"));
 
-		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+		projectsManager.fileChanged(pom.getLocationURI().toString(), CHANGE_TYPE.CHANGED);
+		waitForBackgroundJobs();
 
-		verify(connection, times(1)).sendActionableNotification(any(ActionableNotification.class));
+		ArgumentCaptor<ActionableNotification> argument = ArgumentCaptor.forClass(ActionableNotification.class);
+		verify(connection, times(1)).sendActionableNotification(argument.capture());
+		assertEquals(ClasspathUpdateHandler.CLASSPATH_UPDATED_NOTIFICATION, argument.getValue().getMessage());
+		// Use Paths.get() to normalize the URI: ignore the tailing slash, "/project/path" and "/project/path/" should be the same.
+		assertEquals(Paths.get(project.getLocationURI().toString()), Paths.get((String) argument.getValue().getData()));
+	}
+
+	@Test
+	public void testClasspathUpdateForGradle() throws Exception {
+		importProjects("gradle/simple-gradle");
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("simple-gradle");
+		IFile buildGradle = project.getFile("/build.gradle");
+		assertTrue(buildGradle.exists());
+		ResourceUtils.setContent(buildGradle, ResourceUtils.getContent(buildGradle).replaceAll("org.slf4j:slf4j-api:1.7.21", "org.slf4j:slf4j-api:1.7.20"));
+
+		projectsManager.fileChanged(buildGradle.getLocationURI().toString(), CHANGE_TYPE.CHANGED);
+		waitForBackgroundJobs();
+
+		ArgumentCaptor<ActionableNotification> argument = ArgumentCaptor.forClass(ActionableNotification.class);
+		verify(connection, times(1)).sendActionableNotification(argument.capture());
+		assertEquals(ClasspathUpdateHandler.CLASSPATH_UPDATED_NOTIFICATION, argument.getValue().getMessage());
+		assertEquals(Paths.get(project.getLocationURI().toString()), Paths.get((String) argument.getValue().getData()));
+	}
+
+	@Test
+	public void testClasspathUpdateForEclipse() throws Exception {
+		importProjects("eclipse/updatejar");
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("updatejar");
+		IFile classpath = project.getFile("/.classpath");
+		assertTrue(classpath.exists());
+		ResourceUtils.setContent(classpath, ResourceUtils.getContent(classpath).replaceAll("<classpathentry kind=\"lib\" path=\"lib/foo.jar\"/>", ""));
+
+		projectsManager.fileChanged(classpath.getLocationURI().toString(), CHANGE_TYPE.CHANGED);
+		waitForBackgroundJobs();
+
+		ArgumentCaptor<ActionableNotification> argument = ArgumentCaptor.forClass(ActionableNotification.class);
+		verify(connection, times(1)).sendActionableNotification(argument.capture());
+		assertEquals(ClasspathUpdateHandler.CLASSPATH_UPDATED_NOTIFICATION, argument.getValue().getMessage());
+		assertEquals(Paths.get(project.getLocationURI().toString()), Paths.get((String) argument.getValue().getData()));
+	}
+
+	@Test
+	public void testClasspathUpdateForInvisble() throws Exception {
+		File projectFolder = createSourceFolderWithMissingLibs("dynamicLibDetection");
+		importRootFolder(projectFolder, "Test.java");
+
+		//Add jars to fix compilation errors
+		addLibs(projectFolder.toPath());
+		Path libPath = projectFolder.toPath().resolve(InvisibleProjectBuildSupport.LIB_FOLDER);
+
+		Path jar = libPath.resolve("foo.jar");
+		projectsManager.fileChanged(jar.toUri().toString(), CHANGE_TYPE.CREATED);
+		waitForBackgroundJobs();
+
+		ArgumentCaptor<ActionableNotification> argument = ArgumentCaptor.forClass(ActionableNotification.class);
+		verify(connection, times(1)).sendActionableNotification(argument.capture());
+		assertEquals(ClasspathUpdateHandler.CLASSPATH_UPDATED_NOTIFICATION, argument.getValue().getMessage());
+		assertEquals(Paths.get(projectFolder.toURI().toString()), Paths.get((String) argument.getValue().getData()));
+		
 	}
 }
