e8bdde5870e8ef1863747f50873a4530f4e2a4ac
Fred Bricon <fbricon@gmail.com>
2020-06-17 15:12:43 +0200

    Avoid unnecessary Gradle re-synch on restart
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/managers/GradleProjectImporter.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/managers/GradleProjectImporter.java
@@ -18,12 +18,12 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.buildship.core.BuildConfiguration;
 import org.eclipse.buildship.core.GradleCore;
 import org.eclipse.buildship.core.GradleDistribution;
@@ -44,6 +44,7 @@ import org.eclipse.jdt.ls.core.internal.AbstractProjectImporter;
 import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
 import org.eclipse.jdt.ls.core.internal.ProjectUtils;
 import org.eclipse.jdt.ls.core.internal.preferences.PreferenceManager;
+import org.eclipse.jdt.ls.core.internal.preferences.Preferences;
 import org.eclipse.jdt.ls.internal.gradle.checksums.ValidationResult;
 import org.eclipse.jdt.ls.internal.gradle.checksums.WrapperValidator;
 import org.eclipse.lsp4j.ExecuteCommandParams;
@@ -54,6 +55,7 @@ import org.eclipse.lsp4j.MessageType;
  * @author Fred Bricon
  *
  */
+@SuppressWarnings("restriction")
 public class GradleProjectImporter extends AbstractProjectImporter {
 
 	public static final String GRADLE_HOME = "GRADLE_HOME";
@@ -87,14 +89,15 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 		if (rootFolder == null) {
 			return false;
 		}
-		PreferenceManager preferencesManager = JavaLanguageServerPlugin.getPreferencesManager();
-		if (preferencesManager != null && !preferencesManager.getPreferences().isImportGradleEnabled()) {
+		Preferences preferences = getPreferences();
+		if (!preferences.isImportGradleEnabled()) {
 			return false;
 		}
 		if (directories == null) {
 			BasicFileDetector gradleDetector = new BasicFileDetector(rootFolder.toPath(), BUILD_GRADLE_DESCRIPTOR)
 					.includeNested(false)
-					.addExclusions("**/build");//default gradle build dir
+					.addExclusions("**/build")//default gradle build dir
+					.addExclusions("**/bin");
 			directories = gradleDetector.scan(monitor);
 		}
 		return !directories.isEmpty();
@@ -117,16 +120,18 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 		subMonitor.done();
 	}
 
-	private void importDir(Path rootFolder, IProgressMonitor monitor) {
+	private void importDir(Path projectFolder, IProgressMonitor monitor) {
 		if (monitor.isCanceled()) {
 			return;
 		}
-		startSynchronization(rootFolder, monitor);
+		startSynchronization(projectFolder, monitor);
 	}
+
 
 	public static GradleDistribution getGradleDistribution(Path rootFolder) {
 		PreferenceManager preferencesManager = JavaLanguageServerPlugin.getPreferencesManager();
-		if (preferencesManager != null && preferencesManager.getPreferences().isGradleWrapperEnabled() && Files.exists(rootFolder.resolve("gradlew"))) {
+		Preferences preferences = getPreferences();
+		if (preferencesManager != null && preferences.isGradleWrapperEnabled() && Files.exists(rootFolder.resolve("gradlew"))) {
 			WrapperValidator validator = new WrapperValidator();
 			try {
 				ValidationResult result = validator.checkWrapper(rootFolder.toFile().getAbsolutePath());
@@ -143,7 +148,8 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 								pm.getConnection().sendNotification(params);
 							} else {
 								//@formatter:off
-								String message = GRADLE_WRAPPER_CHEKSUM_WARNING_TEMPLATE.replaceAll("@wrapper@", result.getWrapperJar()).replaceAll("@checksum@", result.getChecksum());
+								String message = GRADLE_WRAPPER_CHEKSUM_WARNING_TEMPLATE.replaceAll("@wrapper@", result.getWrapperJar())
+																						.replaceAll("@checksum@", result.getChecksum());
 								//@formatter:on
 								pm.getConnection().showMessage(new MessageParams(MessageType.Error, message));
 							}
@@ -154,10 +160,10 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 				JavaLanguageServerPlugin.logException(e.getMessage(), e);
 			}
 		}
-		if (preferencesManager != null && preferencesManager.getPreferences().getGradleVersion() != null) {
+		if (StringUtils.isNotBlank(preferences.getGradleVersion())) {
 			List<GradleVersion> versions = CorePlugin.publishedGradleVersions().getVersions();
 			GradleVersion gradleVersion = null;
-			String versionString = preferencesManager.getPreferences().getGradleVersion();
+			String versionString = preferences.getGradleVersion();
 			GradleVersion requiredVersion = GradleVersion.version(versionString);
 			for (GradleVersion version : versions) {
 				if (version.compareTo(requiredVersion) == 0) {
@@ -184,8 +190,9 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 	}
 
 	public static File getGradleHomeFile(Map<String, String> env, Properties sysprops) {
-		if (JavaLanguageServerPlugin.getPreferencesManager() != null && JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleHome() != null) {
-			return new File(JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleHome());
+		Preferences preferences = getPreferences();
+		if (StringUtils.isNotBlank(preferences.getGradleHome())) {
+			return new File(preferences.getGradleHome());
 		}
 		String gradleHome = env.get(GRADLE_HOME);
 		if (gradleHome == null || !new File(gradleHome).isDirectory()) {
@@ -201,9 +208,9 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 	}
 
 	public static File getGradleUserHomeFile() {
-		if (JavaLanguageServerPlugin.getPreferencesManager() != null && JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleUserHome() != null
-				&& !JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleUserHome().isEmpty()) {
-			return new File(JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleUserHome());
+		Preferences preferences = getPreferences();
+		if (StringUtils.isNotBlank(preferences.getGradleUserHome())) {
+			return new File(preferences.getGradleUserHome());
 		}
 		String gradleUserHome = System.getenv().get(GRADLE_USER_HOME);
 		if (gradleUserHome == null) {
@@ -212,19 +219,11 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 		return (gradleUserHome == null || gradleUserHome.isEmpty()) ? null : new File(gradleUserHome);
 	}
 
-	protected void startSynchronization(Path rootFolder, IProgressMonitor monitor) {
-		File location = rootFolder.toFile();
+	protected void startSynchronization(Path projectFolder, IProgressMonitor monitor) {
+		File location = projectFolder.toFile();
 		boolean shouldSynchronize = shouldSynchronize(location);
-		List<IProject> projects = ProjectUtils.getGradleProjects();
-		for (IProject project : projects) {
-			File projectDir = project.getLocation() == null ? null : project.getLocation().toFile();
-			if (location.equals(projectDir)) {
-				shouldSynchronize = checkGradlePersistence(shouldSynchronize, project, projectDir);
-				break;
-			}
-		}
 		if (shouldSynchronize) {
-			BuildConfiguration build = getBuildConfiguration(rootFolder);
+			BuildConfiguration build = getBuildConfiguration(projectFolder);
 			SynchronizationResult result = GradleCore.getWorkspace().createBuild(build).synchronize(monitor);
 			if (!result.getStatus().isOK()) {
 				JavaLanguageServerPlugin.log(result.getStatus());
@@ -236,16 +235,17 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 		GradleDistribution distribution = getGradleDistribution(rootFolder);
 		IVMInstall javaDefaultRuntime = JavaRuntime.getDefaultVMInstall();
 		File javaHome;
+		Preferences preferences = getPreferences();
 		if (javaDefaultRuntime != null && javaDefaultRuntime.getVMRunner(ILaunchManager.RUN_MODE) != null) {
 			javaHome = javaDefaultRuntime.getInstallLocation();
 		} else {
-			String javaHomeStr = JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getJavaHome();
+			String javaHomeStr = preferences.getJavaHome();
 			javaHome = javaHomeStr == null ? null : new File(javaHomeStr);
 		}
 		File gradleUserHome = getGradleUserHomeFile();
-		List<String> gradleArguments = JavaLanguageServerPlugin.getPreferencesManager() != null ? JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleArguments() : new ArrayList<>();
-		List<String> gradleJvmArguments = JavaLanguageServerPlugin.getPreferencesManager() != null ? JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getGradleJvmArguments() : new ArrayList<>();
-		boolean offlineMode = JavaLanguageServerPlugin.getPreferencesManager() != null ? JavaLanguageServerPlugin.getPreferencesManager().getPreferences().isImportGradleOfflineEnabled() : false;
+		List<String> gradleArguments = preferences.getGradleArguments();
+		List<String> gradleJvmArguments = preferences.getGradleJvmArguments();
+		boolean offlineMode = preferences.isImportGradleOfflineEnabled();
 		boolean overrideWorkspaceConfiguration = !(distribution instanceof WrapperGradleDistribution) || offlineMode || (gradleArguments != null && !gradleArguments.isEmpty()) || (gradleJvmArguments != null && !gradleJvmArguments.isEmpty())
 				|| gradleUserHome != null || javaHome != null;
 		// @formatter:off
@@ -263,22 +263,25 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 	}
 
 	public static boolean shouldSynchronize(File location) {
-		boolean shouldSynchronize = true;
-		List<IProject> projects = ProjectUtils.getGradleProjects();
-		for (IProject project : projects) {
+		for (IProject project : ProjectUtils.getGradleProjects()) {
 			File projectDir = project.getLocation() == null ? null : project.getLocation().toFile();
 			if (location.equals(projectDir)) {
-				shouldSynchronize = checkGradlePersistence(shouldSynchronize, project, projectDir);
-				break;
+				boolean shouldSynchronize = checkGradlePersistence(project, projectDir);
+				if (shouldSynchronize) {
+					JavaLanguageServerPlugin.logInfo(project.getName() + " was modified since last time the workspace was opened, must be synchronized");
+				}
+				return shouldSynchronize;
 			}
 		}
-		return shouldSynchronize;
+		JavaLanguageServerPlugin.logInfo("No previous Gradle project at " + location + ", it must be synchronized");
+		return true;
 	}
 
-	private static boolean checkGradlePersistence(boolean shouldSynchronize, IProject project, File projectDir) {
-		if (!ProjectUtils.isJavaProject(project) || !project.getFile(IJavaProject.CLASSPATH_FILE_NAME).exists()) {
+	private static boolean checkGradlePersistence(IProject project, File projectDir) {
+		if (ProjectUtils.isJavaProject(project) && !project.getFile(IJavaProject.CLASSPATH_FILE_NAME).exists()) {
 			return true;
 		}
+		boolean shouldSynchronize = true;
 		PersistentModel model = CorePlugin.modelPersistence().loadModel(project);
 		if (model.isPresent()) {
 			File persistentFile = CorePlugin.getInstance().getStateLocation().append("project-preferences").append(project.getName()).toFile();
@@ -304,6 +307,10 @@ public class GradleProjectImporter extends AbstractProjectImporter {
 
 	@Override
 	public void reset() {
+	}
+
+	private static Preferences getPreferences() {
+		return (JavaLanguageServerPlugin.getPreferencesManager() == null || JavaLanguageServerPlugin.getPreferencesManager().getPreferences() == null) ? new Preferences() : JavaLanguageServerPlugin.getPreferencesManager().getPreferences();
 	}
 
 }
