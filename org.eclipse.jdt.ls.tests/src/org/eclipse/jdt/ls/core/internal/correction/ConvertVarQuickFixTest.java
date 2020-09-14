6ca5602828329ee9370913c2251fb9cd45f894e0
Sheng Chen <sheche@microsoft.com>
2020-09-14 17:14:29 +0800

    Can trigger convert var code action on types
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/correction/ConvertVarQuickFixTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/correction/ConvertVarQuickFixTest.java
@@ -22,9 +22,12 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.ls.core.internal.CodeActionUtil;
 import org.eclipse.jdt.ls.core.internal.WorkspaceHelper;
 import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeActionKind;
 import org.eclipse.lsp4j.Command;
+import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.junit.Before;
 import org.junit.Test;
@@ -59,6 +62,31 @@ public class ConvertVarQuickFixTest extends AbstractQuickFixTest {
 	}
 
 	@Test
+	public void testConvertVarTypeToResolvedType2() throws Exception {
+		IPackageFragment pack1 = sourceFolder.createPackageFragment("foo.bar", false, null);
+		StringBuilder buf = new StringBuilder();
+		buf.append("package foo.bar;\n");
+		buf.append("public class Test {\n");
+		buf.append("    public void test() {\n");
+		buf.append("        var/*cursor*/ name = \"test\";\n");
+		buf.append("    }\n");
+		buf.append("}\n");
+		ICompilationUnit cu = pack1.createCompilationUnit("Test.java", buf.toString(), false, null);
+
+		buf = new StringBuilder();
+		buf.append("package foo.bar;\n");
+		buf.append("public class Test {\n");
+		buf.append("    public void test() {\n");
+		buf.append("        String name = \"test\";\n");
+		buf.append("    }\n");
+		buf.append("}\n");
+
+		Expected expected = new Expected("Change type of 'name' to 'String'", buf.toString(), CodeActionKind.Refactor);
+		Range range = CodeActionUtil.getRange(cu, "/*cursor*/", 0);
+		assertCodeActions(cu, range, expected);
+	}
+
+	@Test
 	public void testConvertResolvedTypeToVar() throws Exception {
 		IPackageFragment pack1 = sourceFolder.createPackageFragment("foo.bar", false, null);
 		StringBuilder buf = new StringBuilder();
@@ -72,6 +100,31 @@ public class ConvertVarQuickFixTest extends AbstractQuickFixTest {
 		List<Either<Command, CodeAction>> commands = evaluateCodeActions(cu);
 		Either<Command, CodeAction> codeAction = commands.stream().filter(c -> getTitle(c).matches("Change type of 'name' to 'var'")).findFirst().orElse(null);
 		assertNotNull(codeAction);
+	}
+
+	@Test
+	public void testConvertResolvedTypeToVar2() throws Exception {
+		IPackageFragment pack1 = sourceFolder.createPackageFragment("foo.bar", false, null);
+		StringBuilder buf = new StringBuilder();
+		buf.append("package foo.bar;\n");
+		buf.append("public class Test {\n");
+		buf.append("    public void test() {\n");
+		buf.append("        String/*cursor*/ name = \"test\";\n");
+		buf.append("    }\n");
+		buf.append("}\n");
+		ICompilationUnit cu = pack1.createCompilationUnit("Test.java", buf.toString(), false, null);
+
+		buf = new StringBuilder();
+		buf.append("package foo.bar;\n");
+		buf.append("public class Test {\n");
+		buf.append("    public void test() {\n");
+		buf.append("        var name = \"test\";\n");
+		buf.append("    }\n");
+		buf.append("}\n");
+
+		Expected expected = new Expected("Change type of 'name' to 'var'", buf.toString(), CodeActionKind.Refactor);
+		Range range = CodeActionUtil.getRange(cu, "/*cursor*/", 0);
+		assertCodeActions(cu, range, expected);
 	}
 
 }
