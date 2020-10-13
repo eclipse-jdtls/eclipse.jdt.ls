b2f8dbded4327529634997cb8287a2c0ac80ca5b
Snjezana Peco <snjezana.peco@redhat.com>
2020-10-13 17:11:49 +0200

    Rename includeGetterSetter to includeAccessors
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/preferences/Preferences.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/preferences/Preferences.java
@@ -61,7 +61,7 @@ public class Preferences {
 	 * Preference key used to include getter, setter and builder/constructor when
 	 * finding references.
 	 */
-	public static final String JAVA_REFERENCES_INCLUDE_GETTERSETTER = "java.references.includeGetterSetter";
+	public static final String JAVA_REFERENCES_INCLUDE_ACCESSORS = "java.references.includeAccessors";
 	/**
 	 * Specifies Java Execution Environments.
 	 */
@@ -439,7 +439,7 @@ public class Preferences {
 	private boolean generateToStringListArrayContents;
 	private int generateToStringLimitElements;
 	private List<String> preferredContentProviderIds;
-	private boolean includeGetterSetter;
+	private boolean includeAccessors;
 
 	private String mavenUserSettings;
 
@@ -636,7 +636,7 @@ public class Preferences {
 		staticImportOnDemandThreshold = IMPORTS_STATIC_ONDEMANDTHRESHOLD_DEFAULT;
 		referencedLibraries = JAVA_PROJECT_REFERENCED_LIBRARIES_DEFAULT;
 		resourceFilters = JAVA_RESOURCE_FILTERS_DEFAULT;
-		includeGetterSetter = false;
+		includeAccessors = true;
 	}
 
 	/**
@@ -881,8 +881,8 @@ public class Preferences {
 		prefs.setFileHeaderTemplate(fileHeader);
 		List<String> typeComment = getList(configuration, JAVA_TEMPLATES_TYPECOMMENT);
 		prefs.setTypeCommentTemplate(typeComment);
-		boolean includeGetterSetter = getBoolean(configuration, JAVA_REFERENCES_INCLUDE_GETTERSETTER, false);
-		prefs.setIncludeGetterSetter(includeGetterSetter);
+		boolean includeAccessors = getBoolean(configuration, JAVA_REFERENCES_INCLUDE_ACCESSORS, true);
+		prefs.setIncludeAccessors(includeAccessors);
 		return prefs;
 	}
 
@@ -1470,12 +1470,12 @@ public class Preferences {
 		return this;
 	}
 
-	public Preferences setIncludeGetterSetter(boolean includeGetterSetter) {
-		this.includeGetterSetter = includeGetterSetter;
+	public Preferences setIncludeAccessors(boolean includeAccessors) {
+		this.includeAccessors = includeAccessors;
 		return this;
 	}
 
-	public boolean isIncludeGetterSetter() {
-		return this.includeGetterSetter;
+	public boolean isIncludeAccessors() {
+		return this.includeAccessors;
 	}
 }
