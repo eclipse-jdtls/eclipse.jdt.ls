b53aaa9c52499669d5dd77309491b31192219841
0dinD <zerodind@gmail.com>
2021-05-02 19:59:31 +0200

    Fix class literal keyword semantic token
    Signed-off-by: 0dinD <zerodind@gmail.com>

---
+++ org.eclipse.jdt.ls.tests/projects/maven/semantic-tokens/src/main/java/foo/ClassLiterals.java
@@ -0,0 +1,14 @@
+package foo;
+
+public class ClassLiterals {
+
+	Class<?> c = String.class;
+
+	public static void main(String[] args) {
+		Class<?> clazz = ClassLiterals.class;
+		if (clazz.) {
+
+		}
+	}
+
+}
