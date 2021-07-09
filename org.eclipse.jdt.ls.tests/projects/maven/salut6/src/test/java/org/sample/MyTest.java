256e705e9ea519f01459215b344099016faa0a62
Snjezana Peco <snjezana.peco@redhat.com>
2021-07-09 21:19:02 +0200

    Organize imports generates duplicate static import statement
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

---
+++ org.eclipse.jdt.ls.tests/projects/maven/salut6/src/test/java/org/sample/MyTest.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test() {
+        assertThat("test", true);
+    }
+}
---
+++ org.eclipse.jdt.ls.tests/projects/maven/salut6/src/test/java/org/sample/MyTest2.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class MyTest2 {
+    @Test
+    public void test() {
+        assertThat("test", true);
+    }
+}
