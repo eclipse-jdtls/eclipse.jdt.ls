6bc0886196cd6178419e6027ead32ef56d49817e
Roland Grunberg <rgrunber@redhat.com>
2021-09-20 11:29:59 -0400

    Update org.jsoup 1.9.2 to 1.14.2
    - StringUtil moved from helper to inernal package
    - Changes in NodeTraversor API
    - Changes to internal field of Cleanup
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/javadoc/HtmlToPlainText.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/javadoc/HtmlToPlainText.java
@@ -23,7 +23,7 @@
  */
 package org.eclipse.jdt.ls.core.internal.javadoc;
 
-import org.jsoup.helper.StringUtil;
+import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
@@ -46,8 +46,7 @@ public class HtmlToPlainText {
 	 */
 	public String getPlainText(Element element) {
 		FormattingVisitor formatter = new FormattingVisitor();
-		NodeTraversor traversor = new NodeTraversor(formatter);
-		traversor.traverse(element); // walk the DOM, and call .head() and .tail() for each node
+		NodeTraversor.traverse(formatter, element); // walk the DOM, and call .head() and .tail() for each node
 
 		return formatter.toString();
 	}
