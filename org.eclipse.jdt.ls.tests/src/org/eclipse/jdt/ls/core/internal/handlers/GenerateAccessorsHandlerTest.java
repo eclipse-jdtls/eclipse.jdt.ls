2130d70af2c39cbf7c6da825f89580456361608a
Shi Chen <chenshi@microsoft.com>
2022-05-23 14:36:02 +0800

    show field type when generating accessors (#2093)
    Signed-off-by: Shi Chen <chenshi@microsoft.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/GenerateAccessorsHandlerTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/handlers/GenerateAccessorsHandlerTest.java
@@ -44,23 +44,32 @@ public class GenerateAccessorsHandlerTest extends AbstractSourceTestCase {
 				"	private static String staticField = \"23434343\";\r\n" +
 				"	private final String finalField;\r\n" +
 				"	String name;\r\n" +
+				"	List<String> names;\r\n" +
 				"}"
 				, true, null);
 		//@formatter:on
 		AccessorField[] accessors = GenerateAccessorsHandler.getUnimplementedAccessors(unit.findPrimaryType(), AccessorKind.BOTH);
 		assertNotNull(accessors);
-		assertEquals(3, accessors.length);
+		assertEquals(4, accessors.length);
 		assertEquals("staticField", accessors[0].fieldName);
 		assertTrue(accessors[0].isStatic);
 		assertTrue(accessors[0].generateGetter && accessors[0].generateSetter);
+		assertEquals("String", accessors[0].typeName);
 		assertEquals("finalField", accessors[1].fieldName);
 		assertFalse(accessors[1].isStatic);
 		assertTrue(accessors[1].generateGetter);
 		assertFalse(accessors[1].generateSetter);
+		assertEquals("String", accessors[1].typeName);
 		assertEquals("name", accessors[2].fieldName);
 		assertFalse(accessors[2].isStatic);
 		assertTrue(accessors[2].generateGetter);
 		assertTrue(accessors[2].generateSetter);
+		assertEquals("String", accessors[2].typeName);
+		assertEquals("names", accessors[3].fieldName);
+		assertFalse(accessors[3].isStatic);
+		assertTrue(accessors[3].generateGetter);
+		assertTrue(accessors[3].generateSetter);
+		assertEquals("List<String>", accessors[3].typeName);
 	}
 
 	@Test
@@ -141,6 +150,7 @@ public class GenerateAccessorsHandlerTest extends AbstractSourceTestCase {
 		assertEquals("id", accessors[0].fieldName);
 		assertFalse(accessors[0].generateGetter);
 		assertTrue(accessors[0].generateSetter);
+		assertEquals("int", accessors[0].typeName);
 	}
 
 	@Test
