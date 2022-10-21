ebf6926daca3aa5d67b21826b31960089712585e
Shi Chen <chenshi@microsoft.com>
2022-10-21 15:36:20 +0800

    support "remove all unused imports"
    Signed-off-by: Shi Chen <chenshi@microsoft.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/CorrectionMessages.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/CorrectionMessages.java
@@ -398,6 +398,7 @@ public final class CorrectionMessages extends NLS {
 	public static String ReorgCorrectionsSubProcessor_accessrules_description;
 	public static String ReorgCorrectionsSubProcessor_project_seup_fix_description;
 	public static String ReorgCorrectionsSubProcessor_project_seup_fix_info;
+	public static String ReorgCorrectionsSubProcessor_remove_all_unused_imports;
 	public static String UnresolvedElementsSubProcessor_change_full_type_description;
 	public static String UnresolvedElementsSubProcessor_copy_annotation_jar_description;
 	public static String UnresolvedElementsSubProcessor_copy_annotation_jar_info;
