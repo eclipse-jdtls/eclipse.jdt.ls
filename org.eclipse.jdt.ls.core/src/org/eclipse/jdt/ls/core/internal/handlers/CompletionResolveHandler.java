b3202c1624bba4522087ab77ccc491007df750c4
Sheng Chen <sheche@microsoft.com>
2023-05-10 10:35:23 +0800

    Remove unnecessary completion item data field
    - The signature information is not needed to store in the completion item
      data field. It can be retrieved from the proposal that stored in the
      completion response cache.
    
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/CompletionResolveHandler.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/CompletionResolveHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016-2021 Red Hat Inc. and others.
+ * Copyright (c) 2016-2023 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -67,13 +67,12 @@ import org.eclipse.osgi.util.NLS;
 import com.google.common.io.CharStreams;
 import com.google.common.util.concurrent.SimpleTimeLimiter;
 import com.google.common.util.concurrent.UncheckedTimeoutException;
+
 /**
  * Adds the completion string and documentation.
  * It checks the client capabilities.
  * If a client supports both SnippetStrings and SignatureHelp
  * SnippetStrings are prioritized for handling parameters.
- *
- *
  */
 @SuppressWarnings("restriction")
 public class CompletionResolveHandler {
@@ -88,9 +87,6 @@ public class CompletionResolveHandler {
 	}
 
 	public static final String DATA_FIELD_URI = "uri";
-	public static final String DATA_FIELD_DECLARATION_SIGNATURE = "decl_signature";
-	public static final String DATA_FIELD_SIGNATURE= "signature";
-	public static final String DATA_FIELD_NAME = "name";
 	public static final String DATA_FIELD_REQUEST_ID = "rid";
 	public static final String DATA_FIELD_PROPOSAL_ID = "pid";
 
@@ -120,11 +116,11 @@ public class CompletionResolveHandler {
 			return param;
 		}
 
+		CompletionProposal proposal = completionResponse.getProposals().get(proposalId);
 		// generic snippets
 		if (param.getKind() == CompletionItemKind.Snippet) {
 			try {
 				CompletionContext ctx = completionResponse.getContext();
-				CompletionProposal proposal = completionResponse.getProposals().get(proposalId);
 				if (proposal instanceof SnippetCompletionProposal) {
 					Template template = ((SnippetCompletionProposal) proposal).getTemplate();
 					String content = SnippetCompletionProposal.evaluateGenericTemplate(unit, ctx, template);
@@ -176,7 +172,7 @@ public class CompletionResolveHandler {
 				manager.getClientPreferences(),
 				true
 			);
-			proposalProvider.updateReplacement(completionResponse.getProposals().get(proposalId), param, '\0');
+			proposalProvider.updateReplacement(proposal, param, '\0');
 		}
 
 		if (!manager.getClientPreferences().isCompletionResolveDocumentSupport()) {
@@ -184,17 +180,29 @@ public class CompletionResolveHandler {
 		}
 
 		// below code is for resolving documentation
-		if (data.containsKey(DATA_FIELD_DECLARATION_SIGNATURE)) {
-			String typeName = stripSignatureToFQN(String.valueOf(data.get(DATA_FIELD_DECLARATION_SIGNATURE)));
-			try {
-				IMember member = null;
-				IType type = unit.getJavaProject().findType(typeName);
-
-				CompletionProposal proposal = completionResponse.getProposals().get(proposalId);
-				if (type!=null && data.containsKey(DATA_FIELD_NAME)) {
-					String name = data.get(DATA_FIELD_NAME);
-					String[] paramSigs = CharOperation.NO_STRINGS;
-					if(data.containsKey( DATA_FIELD_SIGNATURE)){
+		IMember member = null;
+		if (proposal.getKind() == CompletionProposal.TYPE_REF) {
+			char[] signature = proposal.getSignature();
+			if (signature != null) {
+				String typeName = stripSignatureToFQN(String.valueOf(signature));
+				try {
+					IType type = unit.getJavaProject().findType(typeName);
+					if (type != null) {
+						member = type;
+					}
+				} catch (JavaModelException e) {
+					JavaLanguageServerPlugin.logException(e.getMessage(), e);
+					return param;
+				}
+			}
+		} else {
+			char[] declarationSignature = proposal.getDeclarationSignature();
+			if (declarationSignature != null) {
+				String typeName = stripSignatureToFQN(String.valueOf(declarationSignature));
+				try {
+					IType type = unit.getJavaProject().findType(typeName);
+					if (type != null && proposal.getName() != null) {
+						String[] paramSigs = CharOperation.NO_STRINGS;
 						if (proposal instanceof InternalCompletionProposal internalProposal) {
 							Binding binding = internalProposal.getBinding();
 							if (binding instanceof MethodBinding methodBinding) {
@@ -212,114 +220,112 @@ public class CompletionResolveHandler {
 								paramSigs = parameters;
 							}
 						}
-					}
-					IMethod method = type.getMethod(name, paramSigs);
-					IMethod[] methods = type.findMethods(method);
-					if (methods != null && methods.length > 0) {
-						method = methods[0];
-					}
-					if (method.exists()) {
-						member = method;
-					} else {
-						IField field = type.getField(name);
-						if (field.exists()) {
-							member = field;
+						String name = String.valueOf(proposal.getName());
+						IMethod method = type.getMethod(name, paramSigs);
+						IMethod[] methods = type.findMethods(method);
+						if (methods != null && methods.length > 0) {
+							method = methods[0];
+						}
+						if (method.exists()) {
+							member = method;
+						} else {
+							IField field = type.getField(name);
+							if (field.exists()) {
+								member = field;
+							}
 						}
 					}
-				} else {
-					member = type;
+				} catch (JavaModelException e) {
+					JavaLanguageServerPlugin.logException(e.getMessage(), e);
+					return param;
 				}
-
-				if (member != null && member.exists() && !monitor.isCanceled()) {
-					String javadoc = null;
-					try {
-						final IMember curMember = member;
-						javadoc = SimpleTimeLimiter.create(JavaLanguageServerPlugin.getExecutorService()).callWithTimeout(() -> {
-							Reader reader;
-							if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
-								reader = JavadocContentAccess2.getMarkdownContentReader(curMember);
-							} else {
-								reader = JavadocContentAccess.getPlainTextContentReader(curMember);
-							}
-							return reader == null? null:CharStreams.toString(reader);
-						}, 500, TimeUnit.MILLISECONDS);
-					} catch (UncheckedTimeoutException | TimeoutException tooSlow) {
-						//Ignore error for now as it's spamming clients on content assist.
-						//TODO cache javadoc resolution results?
-						//JavaLanguageServerPlugin.logError("Unable to get documentation under 500ms");
-						monitor.setCanceled(true);
-					} catch (Exception e) {
-						JavaLanguageServerPlugin.logException("Unable to read documentation", e);
-						monitor.setCanceled(true);
-					}
-
-					String constantValue = null;
-					if (proposal.getKind() == CompletionProposal.FIELD_REF) {
-						try {
-							IField field = JDTUtils.resolveField(proposal, unit.getJavaProject());
-							Region nameRegion = null;
-							if (field != null) {
-								ITypeRoot typeRoot = field.getTypeRoot();
-								ISourceRange nameRange = ((ISourceReference) field).getNameRange();
-								if (SourceRange.isAvailable(nameRange)) {
-									nameRegion = new Region(nameRange.getOffset(), nameRange.getLength());
-								}
-								constantValue = JDTUtils.getConstantValue(field, typeRoot, nameRegion);
-							}
-						} catch (JavaModelException e) {
-							JavaLanguageServerPlugin.log(e);
-						}
-					}
-					if (constantValue != null) {
-						if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
-							javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + "\n\n" + VALUE + constantValue;
-						} else {
-							javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + VALUE + constantValue;
-						}
-					}
-
-					String defaultValue = null;
-					if (proposal.getKind() == CompletionProposal.METHOD_REF || proposal.getKind() == CompletionProposal.ANNOTATION_ATTRIBUTE_REF) {
-						try {
-							IMethod method = JDTUtils.resolveMethod(proposal, unit.getJavaProject());
-							Region nameRegion = null;
-							if (method != null) {
-								ITypeRoot typeRoot = method.getTypeRoot();
-								ISourceRange nameRange = ((ISourceReference) method).getNameRange();
-								if (SourceRange.isAvailable(nameRange)) {
-									nameRegion = new Region(nameRange.getOffset(), nameRange.getLength());
-								}
-								defaultValue = JDTUtils.getAnnotationMemberDefaultValue(method, typeRoot, nameRegion);
-							}
-						} catch (JavaModelException e) {
-							JavaLanguageServerPlugin.log(e);
-						}
-					}
-					if (defaultValue != null) {
-						if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
-							javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + "\n\n" + DEFAULT + defaultValue;
-						} else {
-							javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + DEFAULT + defaultValue;
-						}
-					}
-					if (javadoc != null) {
-						if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
-							MarkupContent markupContent = new MarkupContent();
-							markupContent.setKind(MarkupKind.MARKDOWN);
-							markupContent.setValue(javadoc);
-							param.setDocumentation(markupContent);
-						} else {
-							param.setDocumentation(javadoc);
-						}
-					}
-				}
-			} catch (JavaModelException e) {
-				JavaLanguageServerPlugin.logException("Unable to resolve compilation", e);
-				monitor.setCanceled(true);
 			}
 		}
-		if (monitor.isCanceled()) {
-			param.setData(null);
+
+		if (member != null && member.exists() && !monitor.isCanceled()) {
+			String javadoc = null;
+			try {
+				final IMember curMember = member;
+				javadoc = SimpleTimeLimiter.create(JavaLanguageServerPlugin.getExecutorService()).callWithTimeout(() -> {
+					Reader reader;
+					if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
+						reader = JavadocContentAccess2.getMarkdownContentReader(curMember);
+					} else {
+						reader = JavadocContentAccess.getPlainTextContentReader(curMember);
+					}
+					return reader == null? null:CharStreams.toString(reader);
+				}, 500, TimeUnit.MILLISECONDS);
+			} catch (UncheckedTimeoutException | TimeoutException tooSlow) {
+				//Ignore error for now as it's spamming clients on content assist.
+				//TODO cache javadoc resolution results?
+				//JavaLanguageServerPlugin.logError("Unable to get documentation under 500ms");
+				return param;
+			} catch (Exception e) {
+				JavaLanguageServerPlugin.logException("Unable to read documentation", e);
+				return param;
+			}
+
+			String constantValue = null;
+			if (proposal.getKind() == CompletionProposal.FIELD_REF) {
+				try {
+					IField field = JDTUtils.resolveField(proposal, unit.getJavaProject());
+					Region nameRegion = null;
+					if (field != null) {
+						ITypeRoot typeRoot = field.getTypeRoot();
+						ISourceRange nameRange = ((ISourceReference) field).getNameRange();
+						if (SourceRange.isAvailable(nameRange)) {
+							nameRegion = new Region(nameRange.getOffset(), nameRange.getLength());
+						}
+						constantValue = JDTUtils.getConstantValue(field, typeRoot, nameRegion);
+					}
+				} catch (JavaModelException e) {
+					JavaLanguageServerPlugin.log(e);
+					return param;
+				}
+			}
+			if (constantValue != null) {
+				if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
+					javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + "\n\n" + VALUE + constantValue;
+				} else {
+					javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + VALUE + constantValue;
+				}
+			}
+
+			String defaultValue = null;
+			if (proposal.getKind() == CompletionProposal.METHOD_REF || proposal.getKind() == CompletionProposal.ANNOTATION_ATTRIBUTE_REF) {
+				try {
+					IMethod method = JDTUtils.resolveMethod(proposal, unit.getJavaProject());
+					Region nameRegion = null;
+					if (method != null) {
+						ITypeRoot typeRoot = method.getTypeRoot();
+						ISourceRange nameRange = ((ISourceReference) method).getNameRange();
+						if (SourceRange.isAvailable(nameRange)) {
+							nameRegion = new Region(nameRange.getOffset(), nameRange.getLength());
+						}
+						defaultValue = JDTUtils.getAnnotationMemberDefaultValue(method, typeRoot, nameRegion);
+					}
+				} catch (JavaModelException e) {
+					JavaLanguageServerPlugin.log(e);
+					return param;
+				}
+			}
+			if (defaultValue != null) {
+				if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
+					javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + "\n\n" + DEFAULT + defaultValue;
+				} else {
+					javadoc = (javadoc == null ? EMPTY_STRING : javadoc) + DEFAULT + defaultValue;
+				}
+			}
+			if (javadoc != null) {
+				if (manager.getClientPreferences().isSupportsCompletionDocumentationMarkdown()) {
+					MarkupContent markupContent = new MarkupContent();
+					markupContent.setKind(MarkupKind.MARKDOWN);
+					markupContent.setValue(javadoc);
+					param.setDocumentation(markupContent);
+				} else {
+					param.setDocumentation(javadoc);
+				}
+			}
 		}
 		return param;
 	}
