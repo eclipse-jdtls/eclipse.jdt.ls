51b34415ef47c953b050a0cb8872cfeb1eeddafd
Morgan Chang <shin19991207@gmail.com>
2025-12-08 15:03:48 -0500

    add one long selection test case
    Signed-off-by: Morgan Chang <shin19991207@gmail.com>

--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/correction/DeprecatedFieldQuickFixTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/correction/DeprecatedFieldQuickFixTest.java
@@ -21,6 +21,7 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.corext.fix.FixMessages;
 import org.eclipse.jdt.ls.core.internal.CodeActionUtil;
+import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 import org.junit.Before;
 import org.junit.Test;
@@ -161,6 +162,66 @@ public class DeprecatedFieldQuickFixTest extends AbstractQuickFixTest {
                 """;
 
         Range selection = CodeActionUtil.getRange(cu, "new Z().field");
+        Expected e = new Expected(FixMessages.ReplaceDeprecatedField_msg, expected);
+        assertCodeActions(cu, selection, e);
+    }
+
+    @Test
+    public void testReplaceDeprecatedField_selectionLengthOne() throws Exception {
+        IPackageFragment pack2 = fSourceFolder.createPackageFragment("test2", false, null);
+        String str2 = """
+                package test2;
+
+                public interface K {
+                    String field1 = "abc";
+                    String field2 = "def";
+                }
+                """;
+        pack2.createCompilationUnit("K.java", str2, false, null);
+
+        IPackageFragment pack1 = fSourceFolder.createPackageFragment("test1", false, null);
+        String str1 = """
+                package test1;
+
+                import test2.K;
+
+                public interface B {
+                    /**
+                     * @deprecated use {@link K#field2} instead
+                     */
+                    @Deprecated
+                    String field = "blah";
+                }
+                """;
+        pack1.createCompilationUnit("B.java", str1, false, null);
+
+        String str = """
+                package test1;
+
+                class E {
+                    public void foo() {
+                        String x = B.field;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        ICompilationUnit cu = pack1.createCompilationUnit("E.java", str, false, null);
+        Position start = CodeActionUtil.getRange(cu, "field").getStart();
+        Range selection = new Range(new Position(start.getLine(), start.getCharacter() + 1), new Position(start.getLine(), start.getCharacter() + 2));
+
+        String expected = """
+                package test1;
+
+                import test2.K;
+
+                class E {
+                    public void foo() {
+                        String x = K.field2;
+                        System.out.println(x);
+                    }
+                }
+                """;
         Expected e = new Expected(FixMessages.ReplaceDeprecatedField_msg, expected);
         assertCodeActions(cu, selection, e);
     }
