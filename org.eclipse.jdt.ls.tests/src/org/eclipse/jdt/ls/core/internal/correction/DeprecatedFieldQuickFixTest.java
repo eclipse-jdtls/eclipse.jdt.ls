43576bd6bcfd1c8430947acfae415f7d4ee93cfc
Morgan Chang <shin19991207@gmail.com>
2025-12-09 06:24:26 -0500

    Migrate new "Replace deprecated field" QuickFix from jdt.ui (#3637)
    Co-authored-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/correction/DeprecatedFieldQuickFixTest.java
@@ -0,0 +1,228 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.ls.core.internal.correction;
+
+import java.util.Map;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.corext.fix.FixMessages;
+import org.eclipse.jdt.ls.core.internal.CodeActionUtil;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeprecatedFieldQuickFixTest extends AbstractQuickFixTest {
+
+    private IJavaProject fJProject;
+    private IPackageFragmentRoot fSourceFolder;
+
+    @Before
+    public void setup() throws Exception {
+        fJProject = newEmptyProject();
+		Map<String, String> options = TestOptions.getDefaultOptions();
+        options.put(JavaCore.COMPILER_PB_DEPRECATION, JavaCore.WARNING);
+        fJProject.setOptions(options);
+        fSourceFolder = fJProject.getPackageFragmentRoot(fJProject.getProject().getFolder("src"));
+    }
+
+    @Test
+    public void testReplaceDeprecatedField_externalInterface_staticAccess() throws Exception {
+        IPackageFragment pack2 = fSourceFolder.createPackageFragment("test2", false, null);
+        String str2 = """
+                package test2;
+
+                public interface K {
+                    String field1 = "abc";
+                    String field2 = "def";
+                }
+                """;
+        pack2.createCompilationUnit("K.java", str2, false, null);
+
+        IPackageFragment pack1 = fSourceFolder.createPackageFragment("test1", false, null);
+        String str1 = """
+                package test1;
+
+                import test2.K;
+
+                public interface B {
+                    /**
+                     * @deprecated use {@link K#field2} instead
+                     */
+                    @Deprecated
+                    String field = "blah";
+                }
+                """;
+        pack1.createCompilationUnit("B.java", str1, false, null);
+
+        String str = """
+                package test1;
+
+                class E {
+                    public void foo() {
+                        String x = B.field;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        ICompilationUnit cu = pack1.createCompilationUnit("E.java", str, false, null);
+
+        String expected = """
+                package test1;
+
+                import test2.K;
+
+                class E {
+                    public void foo() {
+                        String x = K.field2;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        Range selection = CodeActionUtil.getRange(cu, "B.field");
+        Expected e = new Expected(FixMessages.ReplaceDeprecatedField_msg, expected);
+        assertCodeActions(cu, selection, e);
+    }
+
+    @Test
+    public void testReplaceDeprecatedField_externalInterface_innerClassAccess() throws Exception {
+        IPackageFragment pack2 = fSourceFolder.createPackageFragment("test2", false, null);
+        String str2 = """
+                package test2;
+
+                public interface K {
+                    String field1 = "abc";
+                    String field2 = "def";
+                }
+                """;
+        pack2.createCompilationUnit("K.java", str2, false, null);
+
+        IPackageFragment pack1 = fSourceFolder.createPackageFragment("test1", false, null);
+        String str1 = """
+                package test1;
+
+                import test2.K;
+
+                public interface B {
+                    /**
+                     * @deprecated use {@link K#field2} instead
+                     */
+                    @Deprecated
+                    String field = "blah";
+                }
+                """;
+        pack1.createCompilationUnit("B.java", str1, false, null);
+
+        String str = """
+                package test1;
+
+                class E {
+                    private class Z implements B {
+                    }
+
+                    public void foo() {
+                        String x = new Z().field;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        ICompilationUnit cu = pack1.createCompilationUnit("E.java", str, false, null);
+
+        String expected = """
+                package test1;
+
+                import test2.K;
+
+                class E {
+                    private class Z implements B {
+                    }
+
+                    public void foo() {
+                        String x = K.field2;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        Range selection = CodeActionUtil.getRange(cu, "new Z().field");
+        Expected e = new Expected(FixMessages.ReplaceDeprecatedField_msg, expected);
+        assertCodeActions(cu, selection, e);
+    }
+
+    @Test
+    public void testReplaceDeprecatedField_selectionLengthOne() throws Exception {
+        IPackageFragment pack2 = fSourceFolder.createPackageFragment("test2", false, null);
+        String str2 = """
+                package test2;
+
+                public interface K {
+                    String field1 = "abc";
+                    String field2 = "def";
+                }
+                """;
+        pack2.createCompilationUnit("K.java", str2, false, null);
+
+        IPackageFragment pack1 = fSourceFolder.createPackageFragment("test1", false, null);
+        String str1 = """
+                package test1;
+
+                import test2.K;
+
+                public interface B {
+                    /**
+                     * @deprecated use {@link K#field2} instead
+                     */
+                    @Deprecated
+                    String field = "blah";
+                }
+                """;
+        pack1.createCompilationUnit("B.java", str1, false, null);
+
+        String str = """
+                package test1;
+
+                class E {
+                    public void foo() {
+                        String x = B.field;
+                        System.out.println(x);
+                    }
+                }
+                """;
+
+        ICompilationUnit cu = pack1.createCompilationUnit("E.java", str, false, null);
+        Position start = CodeActionUtil.getRange(cu, "field").getStart();
+        Range selection = new Range(new Position(start.getLine(), start.getCharacter() + 1), new Position(start.getLine(), start.getCharacter() + 2));
+
+        String expected = """
+                package test1;
+
+                import test2.K;
+
+                class E {
+                    public void foo() {
+                        String x = K.field2;
+                        System.out.println(x);
+                    }
+                }
+                """;
+        Expected e = new Expected(FixMessages.ReplaceDeprecatedField_msg, expected);
+        assertCodeActions(cu, selection, e);
+    }
+}
