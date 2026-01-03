b4ed44c086c705b3f7b621345f32647d3d0733a6
Suby S Surendran <suby.surendran@ibm.com>
2026-01-03 18:43:08 +0530

    {@link #m()} should be handled

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/javadoc/JavadocContentAccess2.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/javadoc/JavadocContentAccess2.java
@@ -228,6 +228,7 @@ public class JavadocContentAccess2 {
 		String refMemberName = null;
 		String[] refMethodParamTypes = null;
 		String[] refMethodParamNames = null;
+		boolean hasParentheses = false;
 		if (e instanceof Name) {
 			Name name = (Name) e;
 			refTypeName = name.getFullyQualifiedName();
@@ -241,6 +242,9 @@ public class JavadocContentAccess2 {
 			Name qualifier = methodRef.getQualifier();
 			refTypeName = qualifier == null ? "" : qualifier.getFullyQualifiedName(); //$NON-NLS-1$
 			refMemberName = methodRef.getName().getIdentifier();
+			if (refMemberName != null) {
+				hasParentheses = e.toString().endsWith("()");
+			}
 			@SuppressWarnings("unchecked")
 			List<MethodRefParameter> params = methodRef.parameters();
 			int ps = params.size();
@@ -260,7 +264,11 @@ public class JavadocContentAccess2 {
 		List<String> result = new ArrayList<>();
 		result.add(refTypeName);
 		if (refMemberName != null) {
-			result.add(refMemberName);
+			if (hasParentheses) {
+				result.add(refMemberName + "()");
+			} else {
+				result.add(refMemberName);
+			}
 		}
 		if (refMethodParamTypes != null) {
 			result.addAll(Arrays.asList(refMethodParamTypes));
