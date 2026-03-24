957eddaf0c593ff00891ffc005d4168350f4b5ee
Jeff Johnston <jjohnstn@redhat.com>
2026-03-24 16:15:32 -0400

    Simplify to not look at settings URL and just change project compliance

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/proposals/ChangeToRequiredCompilerCompliance.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/proposals/ChangeToRequiredCompilerCompliance.java
@@ -12,34 +12,19 @@
  *******************************************************************************/
 package org.eclipse.jdt.ls.core.internal.corrections.proposals;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.URIUtil;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.manipulation.ChangeCorrectionProposalCore;
 import org.eclipse.jdt.internal.corext.util.Messages;
-import org.eclipse.jdt.ls.core.internal.ExternalFileChange;
-import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
-import org.eclipse.jdt.ls.core.internal.ResourceUtils;
 import org.eclipse.jdt.ls.core.internal.commands.ProjectCommand;
 import org.eclipse.jdt.ls.core.internal.corrections.CorrectionMessages;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.ltk.core.refactoring.Change;
-import org.eclipse.text.edits.ReplaceEdit;
 
 public final class ChangeToRequiredCompilerCompliance extends ChangeCorrectionProposalCore {
 
@@ -75,73 +60,27 @@ public final class ChangeToRequiredCompilerCompliance extends ChangeCorrectionPr
 	 */
 	@Override
 	protected Change createChange() throws CoreException {
-		URI settingsURI = JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getSettingsAsURI();
-		File settingsFile = null;
-		boolean projectSettings = false;
-		try {
-			if (settingsURI != null && URIUtil.isFileURI(settingsURI)) {
-				settingsFile = ResourceUtils.toFile(settingsURI);
-			} else {
-				String settingsUrl = JavaLanguageServerPlugin.getPreferencesManager().getPreferences().getSettingsUrl();
-				settingsURI = new URI("file://" + fProject.getProject().getRawLocation() + "/" + settingsUrl); //$NON-NLS-1$ //$NON-NLS-2$
-				if (settingsURI != null && URIUtil.isFileURI(settingsURI)) {
-					settingsFile = ResourceUtils.toFile(settingsURI);
-					projectSettings = true;
-				}
-			}
-			if (settingsFile != null && settingsFile.exists()) {
-				Map<String, Object> options = new LinkedHashMap<>();
-				if (!projectSettings) {
-					options = readOptions(settingsFile);
-				}
-				options.put(JavaCore.COMPILER_COMPLIANCE, fRequiredVersion);
-				options.put(JavaCore.COMPILER_SOURCE, fRequiredVersion);
-				options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, fRequiredVersion);
-				options.put(JavaCore.COMPILER_PB_ASSERT_IDENTIFIER, JavaCore.ERROR);
-				options.put(JavaCore.COMPILER_PB_ENUM_IDENTIFIER, JavaCore.ERROR);
-				options.put(JavaCore.COMPILER_RELEASE, JavaCore.ENABLED);
-				if (fEnablePreviews) {
-					options.put(JavaCore.COMPILER_PB_ENABLE_PREVIEW_FEATURES, JavaCore.ENABLED);
-					options.put(JavaCore.COMPILER_PB_REPORT_PREVIEW_FEATURES, JavaCore.IGNORE);
-				} else {
-					options.put(JavaCore.COMPILER_PB_ENABLE_PREVIEW_FEATURES, JavaCore.DISABLED);
-					options.put(JavaCore.COMPILER_PB_REPORT_PREVIEW_FEATURES, JavaCore.WARNING);
-				}
-				if (projectSettings) {
-					try {
-						URI projectUri = fProject.getProject().getLocationURI();
-						ProjectCommand.updateProjectSettings(projectUri.toString(), options);
-					} catch (Exception e) {
-						// continue
-					}
-					return null;
-				} else {
-					String content = ResourceUtils.getContent(settingsURI);
-					IDocument doc = new Document(content);
-					ExternalFileChange editChange = new ExternalFileChange(fName, doc, settingsURI);
-					ReplaceEdit edit = new ReplaceEdit(0, content.length(), options.entrySet().stream().map(entry -> entry.getKey() + "=" + entry.getValue() + "\n").collect(Collectors.joining()));
-					editChange.setEdit(edit);
-					return editChange;
-				}
-			}
-		} catch (IOException | URISyntaxException e) {
-			throw new CoreException(Status.error(CorrectionMessages.UnexpectedError, e));
-		}
-		return super.createChange();
-	}
-
-	private Map<String, Object> readOptions(File file) throws FileNotFoundException, IOException {
 		Map<String, Object> options = new LinkedHashMap<>();
-		try (FileReader reader = new FileReader(file); BufferedReader bf = new BufferedReader(reader);) {
-			String line = null;
-			while ((line = bf.readLine()) != null) {
-				String[] parts = line.split("="); //$NON-NLS-1$
-				if (parts.length == 2) {
-					options.put(parts[0], parts[1]);
-				}
-			}
+		options.put(JavaCore.COMPILER_COMPLIANCE, fRequiredVersion);
+		options.put(JavaCore.COMPILER_SOURCE, fRequiredVersion);
+		options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, fRequiredVersion);
+		options.put(JavaCore.COMPILER_PB_ASSERT_IDENTIFIER, JavaCore.ERROR);
+		options.put(JavaCore.COMPILER_PB_ENUM_IDENTIFIER, JavaCore.ERROR);
+		options.put(JavaCore.COMPILER_RELEASE, JavaCore.ENABLED);
+		if (fEnablePreviews) {
+			options.put(JavaCore.COMPILER_PB_ENABLE_PREVIEW_FEATURES, JavaCore.ENABLED);
+			options.put(JavaCore.COMPILER_PB_REPORT_PREVIEW_FEATURES, JavaCore.IGNORE);
+		} else {
+			options.put(JavaCore.COMPILER_PB_ENABLE_PREVIEW_FEATURES, JavaCore.DISABLED);
+			options.put(JavaCore.COMPILER_PB_REPORT_PREVIEW_FEATURES, JavaCore.WARNING);
 		}
-		return options;
+		try {
+			URI projectUri = fProject.getProject().getLocationURI();
+			ProjectCommand.updateProjectSettings(projectUri.toString(), options);
+		} catch (Exception e) {
+			// continue
+		}
+		return null;
 	}
 
 }
