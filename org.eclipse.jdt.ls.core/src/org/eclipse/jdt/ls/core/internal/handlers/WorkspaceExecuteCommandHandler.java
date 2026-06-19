769987b502c4a4bdb767add0612fea62bd4ef47f
wenyutang-ms <wenyutang@microsoft.com>
2026-06-19 08:28:16 +0800

    Revert "Refresh command listing without resetting handlers"
    This reverts commit dda8e81c3c6348a358b7877094cb7685865d4aad.

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/WorkspaceExecuteCommandHandler.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/WorkspaceExecuteCommandHandler.java
@@ -170,7 +170,7 @@ public class WorkspaceExecuteCommandHandler implements IRegistryEventListener {
 	}
 
 	public Set<String> getAllCommands() {
-		Collection<DelegateCommandHandlerDescriptor> handlers = getDelegateCommandHandlerDescriptors(true);
+		Collection<DelegateCommandHandlerDescriptor> handlers = getDelegateCommandHandlerDescriptors();
 		Set<String> commands = new HashSet<>();
 		for (DelegateCommandHandlerDescriptor handler : handlers) {
 			commands.addAll(handler.getAllCommands());
