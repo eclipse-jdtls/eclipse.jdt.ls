68d43aee217251a61bbb51c3a490015c1aba7406
Morgan Chang <shin19991207@gmail.com>
2026-08-07 16:20:50 -0400

    Support confirmation of move instance method refactoring errors
    Signed-off-by: Morgan Chang <shin19991207@gmail.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/GetRefactorEditHandler.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/GetRefactorEditHandler.java
@@ -309,6 +309,8 @@ public class GetRefactorEditHandler {
 		 */
 		public Command command;
 		public String errorMessage;
+		public Boolean canContinue;
+		public String confirmationToken;
 
 		public RefactorWorkspaceEdit(WorkspaceEdit edit) {
 			this.edit = edit;
@@ -321,6 +323,12 @@ public class GetRefactorEditHandler {
 
 		public RefactorWorkspaceEdit(String errorMessage) {
 			this.errorMessage = errorMessage;
+		}
+
+		public RefactorWorkspaceEdit(String errorMessage, boolean canContinue, String confirmationToken) {
+			this.errorMessage = errorMessage;
+			this.canContinue = canContinue;
+			this.confirmationToken = confirmationToken;
 		}
 	}
 
