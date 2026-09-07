dc126eb84524097dfe902ce982c2bfaaf7c05995
Wahyu <wahyu896452@gmail.com>
2026-09-07 11:36:00 +0700

    dev: delete unnecessary comment

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/decompiler/DecompilerImpl.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/decompiler/DecompilerImpl.java
@@ -29,11 +29,7 @@ import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
 public abstract class DecompilerImpl implements IDecompiler {
 	private static Map<DecompilerType, Map<String, DecompilerResult>> decompilerCache =
 			Collections.synchronizedMap(new EnumMap<>(DecompilerType.class));
-	/**
-	 * Maximum entries in the per-type decompiler LRU cache.
-	 * Configurable via {@code -Djdt.ls.decompiler.cacheSize=<int>}.
-	 * Default: 100
-	 */
+
 	private static final int CACHE_SIZE = Integer.getInteger("jdt.ls.decompiler.cacheSize", 100);
 
 	private static Map<String, DecompilerResult> getLRUCache(int maxEntries) {
