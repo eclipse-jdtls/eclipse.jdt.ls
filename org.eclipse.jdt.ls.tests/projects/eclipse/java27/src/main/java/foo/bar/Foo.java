3c32fd1ccc9e2e245f9b0fe9139b1511abc727d0
MEHER SRUJANA MATCHA <mehersrujana.matcha@ibm.com>
2026-09-16 22:23:25 +0100

    Add tests for Java 27 support (#3889)

---
+++ org.eclipse.jdt.ls.tests/projects/eclipse/java27/src/main/java/foo/bar/Foo.java
@@ -0,0 +1,15 @@
+package foo.bar;
+
+/**
+ * It's a Foo class
+ */
+public class Foo {
+	// JEP 532: Primitive Types in Patterns, instanceof, and switch (Fifth Preview)
+	static String classify(int value) {
+		return switch (value) {
+			case 0 -> "zero";
+			case int i when i > 0 -> "positive";
+			case int i -> "negative";
+		};
+	}
+}
--- org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/managers/EclipseProjectImporterTest.java
+++ org.eclipse.jdt.ls.tests/src/org/eclipse/jdt/ls/core/internal/managers/EclipseProjectImporterTest.java
@@ -199,7 +199,7 @@ public class EclipseProjectImporterTest extends AbstractProjectsManagerBasedTest
 
 	@Test
 	public void testPreviewFeaturesDisabledByDefault() throws Exception {
-		String name = "java26";
+		String name = "java27";
 		importProjects("eclipse/" + name);
 		IProject project = getProject(name);
 		assertIsJavaProject(project);
